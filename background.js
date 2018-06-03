79ba82982dfe09738d5d650ee03d16f8d90987bb
Eemeli Aro <eemeli@gmail.com>
2018-06-03 14:35:07 +0300

    First version

---
+++ background.js
@@ -0,0 +1,14 @@
+const code = `(() => {
+  const elements = document.querySelectorAll('body *')
+  for (let i = 0; i < elements.length; i++) {
+    const el = elements[i]
+    const pos = window.getComputedStyle(el).position
+    if (pos === 'fixed' || pos === 'sticky') {
+      el.parentNode.removeChild(el)
+    }
+  }
+})()`
+
+chrome.browserAction.onClicked.addListener(() => {
+  chrome.tabs.executeScript({ code })
+})
